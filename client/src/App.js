3ebcd4b18b1fd9cc87cd79b9f8c747bfaf53b62b
tmtcam-042 <90966515+tmtcam-042@users.noreply.github.com>
2022-04-10 22:53:24 +1000

    Created basic frontend functionality. Error where json response is obtained in server but not properly processed clientside, to be worked on.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [tags, setTags] = React.useState('');
+  const [picArray, setPicArray] = React.useState(null);
+
+  const getPicArray = () => {
+    fetch('/api/search/' + tags)
+      .then(response => {
+        console.log('Request received! Result:\n');
+        console.log(response);
+        let pictures = response.json().photo.map((pic) => {
+          var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+          return(
+            <img alt={pic.title} src={srcPath}></img>
+          )
+        })
+      })
+      .then(response => setPicArray(response))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Flickr Search</h1>
+      <input placeholder="cats, cute, fluffy" value={tags} onChange={event => setTags(event.target.value.split(" "))} /> {/* TODO: make input processing more resilient. */}
+      <button onClick={getPicArray}>Search</button>
+      <div>
+        {picArray}
+      </div>
     </div>
   );
 }
